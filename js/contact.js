95c2800ef8efeff07b732cccdbbd7e728d633bf5
sanket-rajgarhia <sanket.rajgarhia@gmail.com>
2020-10-19 19:21:02 +0530

    Fixed header in landscape orientation and added barebone recaptcha.

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,3 @@
-
 /* Adding tooltips to the form controls */
 $(document).ready(function() {
 
@@ -20,49 +19,11 @@ let form = document.querySelector('.needs-validation');
 // Add event listener to the form object when submit button is clicked.
 form.addEventListener('submit', validateForm);
 
-function validateForm(event){
-  if (form.checkValidity() === false)
-  {
+function validateForm(event) {
+  if (form.checkValidity() === false) {
     console.log("here");
     event.preventDefault();
     event.stopPropagation();
   }
   form.classList.add('was-validated');
 }
-
-$(function () {
-
-    window.verifyRecaptchaCallback = function (response) {
-        $('input[data-recaptcha]').val(response).trigger('change');
-    };
-
-    window.expiredRecaptchaCallback = function () {
-        $('input[data-recaptcha]').val("").trigger('change');
-    };
-
-    $('#contact-form').validator();
-
-    $('#contact-form').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.html";
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data) {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                        grecaptcha.reset();
-                    }
-                }
-            });
-            return false;
-        }
-    });
-});
