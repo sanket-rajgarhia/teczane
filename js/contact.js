55d8e4b6cfcb5e5e2de47495157d05f106b3255a
sanket-rajgarhia <sanket.rajgarhia@gmail.com>
2020-10-20 19:46:33 +0530

    Successfully implemented recaptcha v2 and made visual adjustments.

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,5 @@
 /* Adding tooltips to the form controls */
+/* ###########################################################################*/
 $(document).ready(function() {
 
   $('.hastooltip').tooltip();
@@ -12,28 +13,54 @@ $(document).ready(function() {
 
 });
 
-/* Form validation */
+/* Form validation and Recaptcha validation */
+/* ###########################################################################*/
 // Fetch the reference to the form that needs validation
 let form = document.querySelector('.needs-validation');
 
 // Add event listener to the form object when submit button is clicked.
 form.addEventListener('submit', validateForm);
 
+/* Callback function to perform validation when 'submit' button is clicked */
 function validateForm(event) {
-  let returnFlag = true;
+
+  // Flags that are true if form control validity and recaptcha validity passes successfully
+  let validityFlag = true;
+  let captchaFlag = true;
+
+  // Validate form input controls
   if (form.checkValidity() === false) {
     event.preventDefault();
     event.stopPropagation();
+
     returnFlag = false;
   }
-  else{
-    let response = grecaptcha.getResponse();
-    if(response.length == 0) {
-      event.preventDefault();
-      event.stopPropagation();
-      returnFlag = false;
-    }
+
+  // Validate recaptcha v2
+  let response = grecaptcha.getResponse();
+  if (response.length == 0) {
+    event.preventDefault();
+    event.stopPropagation();
+    captchaFlag = false;
   }
+
   form.classList.add('was-validated');
-  return returnFlag;
+
+  // Return true only if both validation passes
+  if (validityFlag === false && captchaFlag === false) {
+    return false;
+  } else {
+    return validityFlag && captchaFlag;
+  }
+
+}
+
+/* Callback fucntion for recaptcha - data-callback  when recaptcha response is a success */
+function verifyRecaptchaCallback(response) {
+  $('input[data-recaptcha]').val(response).trigger('change');
+}
+
+/* Callback fucntion for recaptcha - data-expired-callbackk when recaptcha v2 expires */
+function expiredRecaptchaCallback() {
+  $('input[data-recaptcha]').val("").trigger('change');
 }
